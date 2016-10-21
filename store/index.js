82b02dc8ab46b7fefba73e81b80f3155a26f7e9a
Jef King <jefkin@microsoft.com>
2016-10-21 15:13:32 -0600

    2 doc db

--- store/index.js
+++ store/index.js
@@ -5,8 +5,6 @@ var config = require('./config');
 
 appInsights.setup(config.AppInsightsKey).start();
 
-//Store data from service bus messages
 module.exports = function(context, message) {
-    context.log('Node.js ServiceBus queue trigger function processed message', message);
     context.done();
 };
