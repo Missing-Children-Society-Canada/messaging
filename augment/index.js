018d4d8ba69bd809b8dfc22b7f4a45955fa3a268
Jef King <jefkin@microsoft.com>
2016-10-21 14:00:23 -0600

    kill it

--- augment/index.js
+++ augment/index.js
@@ -6,14 +6,3 @@ appInsights.setup(config.AppInsightsKey).start();
 
 //add extraa data to messages
 //sentiment analysis?
-var serviceBusService = azure.createServiceBusService(config.ServiceBusConnection);
-
-serviceBusService.receiveQueueMessage(config.QueueName, function (error, receivedMessage) {
-    if (error) {
-        appInsights.client.trackException(error);
-    }
-    else
-    {
-     //Work with message   
-    }
-});
--- filter/index.js
+++ filter/index.js
@@ -5,14 +5,3 @@ var config = require('./config');
 appInsights.setup(config.AppInsightsKey).start();
 
 //confirm messasges should be processed
-var serviceBusService = azure.createServiceBusService(config.ServiceBusConnection);
-
-serviceBusService.receiveQueueMessage(config.QueueName, function (error, receivedMessage) {
-    if (error) {
-        appInsights.client.trackException(error);
-    }
-    else
-    {
-     //Work with message   
-    }
-});
