fda0cba7e9b9725a86d1597419ff4260e0bc75e7
Jef King <jefkin@microsoft.com>
2016-10-21 13:31:44 -0600

    functions

---
+++ filter/config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.AppInsightsKey = 'c31db7e0-5df2-44ad-9e76-892af521eecf';
+
+config.ServiceBusConnection = 'Endpoint=sb://asgasdsdasdasdas.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=h4doSwMPpuzjiEf/CELZICfpNlIlYhaMS0xK/oYooAI=';
+config.QueueName = 'filter';
+
+module.exports = config;
