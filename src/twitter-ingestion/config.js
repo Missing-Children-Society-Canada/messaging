5630eae69891d8b0fa8fafe8fe574fb6487deed0
Jef King <jefkin@microsoft.com>
2016-10-22 13:43:06 -0600

    part of Functions

--- src/twitter-ingestion/config.js
+++ src/twitter-ingestion/config.js
@@ -5,8 +5,6 @@ config.ConsumerSecret = process.env.TWITTER_CONSUMER_SECRET;
 config.AccessToken = process.env.TWITTER_ACCESS_TOKEN;
 config.AccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 
-config.ServiceBusConnection = process.env.SERVICE_BUS_CONNECTION;
-
 config.AppInsightsKey = process.env.APP_INSIGHTS_KEY;
 
 config.HashTag = process.env.HASH_TAG;
