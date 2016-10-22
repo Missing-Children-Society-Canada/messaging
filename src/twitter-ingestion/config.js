28605c01fe97c74840aed8a9f055a8e9b49071a2
Jef King <jefkin@microsoft.com>
2016-10-22 14:51:44 -0600

    deploy

--- src/twitter-ingestion/config.js
+++ src/twitter-ingestion/config.js
@@ -1,10 +1,10 @@
 var config = {}
 
 //TWITTER CONFIG
-config.ConsumerKey = process.env.TWITTER_CONSUMER_KEY;
-config.ConsumerSecret = process.env.TWITTER_CONSUMER_SECRET;
-config.AccessToken = process.env.TWITTER_ACCESS_TOKEN;
-config.AccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
-config.HashTag = process.env.HASH_TAG;
+config.ConsumerKey = process.env.TWITTER_CONSUMER_KEY || 'XXXXXXXXXXXXXXXX';
+config.ConsumerSecret = process.env.TWITTER_CONSUMER_SECRET || 'XXXXXXXXXXXXXXXX';
+config.AccessToken = process.env.TWITTER_ACCESS_TOKEN || 'XXXXXXXXXXXXXXXX';
+config.AccessTokenSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET || 'XXXXXXXXXXXXXXXX';
+config.HashTag = process.env.HASH_TAG || 'helpmeplease';
 
 module.exports = config;
