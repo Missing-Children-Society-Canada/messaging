6e20703c6472f5a8b4cfc12245e335d4aae0b71d
Jef King <jefkin@microsoft.com>
2016-10-23 09:33:57 -0600

    Pull config

--- src/augment/index.js
+++ src/augment/index.js
@@ -1,3 +1,12 @@
+var util = require('util'),
+    twitter = require('twitter');
+var twit = new twitter({
+    consumer_key: process.env.TwitterConsumerKey,
+    consumer_secret: process.env.ConsumerSecret,
+    access_token_key: process.env.AccessTokenKey,
+    access_token_secret: process.env.AccessTokenSecret
+});
+
 module.exports = function(context, message) {
     //Pull Lat/Long from Twitter, based on Tweet
     message.latitude = -115.00002;
