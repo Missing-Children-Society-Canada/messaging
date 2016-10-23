88429f44f2db0ce22807c7afd85e3e1a52646174
Jef King <jefkin@microsoft.com>
2016-10-22 20:04:45 -0600

    misc

--- src/twitter-ingestion/index.js
+++ src/twitter-ingestion/index.js
@@ -1,29 +1,29 @@
 var Twitter = require('./twitter');
 
-//TWITTER CONFIG
-var config = {
-    ConsumerKey = GetEnvironmentVariable('TWITTER_CONSUMER_KEY'),
-    ConsumerSecret = GetEnvironmentVariable('TWITTER_CONSUMER_SECRET'),
-    AccessToken = GetEnvironmentVariable('TWITTER_ACCESS_TOKEN'),
-    AccessTokenSecret = GetEnvironmentVariable('TWITTER_ACCESS_TOKEN_SECRET'),
-    HashTag = GetEnvironmentVariable('HASH_TAG')
-};
-
-var twitterClient = new Twitter({
-    consumer_key: config.ConsumerKey,
-    consumer_secret: config.ConsumerSecret,
-    access_token: config.AccessToken,
-    access_token_secret: config.AccessTokenSecret,
-    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-});
-
-var stream = twitterClient.stream('statuses/filter', {
-    track: [config.HashTag]
-});
 
 module.exports = function (context) {
 
+    //TWITTER CONFIG
+    var config = {
+        ConsumerKey: GetEnvironmentVariable('TWITTER_CONSUMER_KEY'),
+        ConsumerSecret: GetEnvironmentVariable('TWITTER_CONSUMER_SECRET'),
+        AccessToken: GetEnvironmentVariable('TWITTER_ACCESS_TOKEN'),
+        AccessTokenSecret: GetEnvironmentVariable('TWITTER_ACCESS_TOKEN_SECRET'),
+        HashTag: GetEnvironmentVariable('HASH_TAG')
+    };
     context.log(config);
+
+    var twitterClient = new Twitter({
+        consumer_key: config.ConsumerKey,
+        consumer_secret: config.ConsumerSecret,
+        access_token: config.AccessToken,
+        access_token_secret: config.AccessTokenSecret,
+        timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    });
+
+    var stream = twitterClient.stream('statuses/filter', {
+        track: [config.HashTag]
+    });
 
     stream.on('tweet', function (tweet) {
         context.bindings.ingestion = {
@@ -32,3 +32,8 @@ module.exports = function (context) {
         };
     });
 };
+
+function GetEnvironmentVariable(name)
+{
+    return process.env[name];
+}
