cd9c3d98b7fd1752bd9ecb2f75750a8ccd658ad2
Jef King <jefkin@microsoft.com>
2016-10-22 18:31:05 -0600

    try that

--- src/twitter-ingestion/index.js
+++ src/twitter-ingestion/index.js
@@ -1,15 +1,15 @@
 var twitterClient = require('./twitter');
 var config = require('./config');
 
-module.exports = function (context) {
+var twitterClient = new twitterClient({
+    consumer_key: config.ConsumerKey,
+    consumer_secret: config.ConsumerSecret,
+    access_token: config.AccessToken,
+    access_token_secret: config.AccessTokenSecret,
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+});
 
-    var twitterClient = new twitterClient({
-        consumer_key: config.ConsumerKey,
-        consumer_secret: config.ConsumerSecret,
-        access_token: config.AccessToken,
-        access_token_secret: config.AccessTokenSecret,
-        timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    });
+module.exports = function (context) {
 
     var stream = twitterClient.stream('statuses/filter', {
         track: [config.HashTag]
