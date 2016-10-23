6d3e1eb87e8e8bef4b88fe66fa1e6849f6614619
Jef King <jefkin@microsoft.com>
2016-10-22 19:48:23 -0600

    should work

--- src/twitter-ingestion/index.js
+++ src/twitter-ingestion/index.js
@@ -1,7 +1,14 @@
-var twitterClient = require('./twitter');
-var config = require('./config');
+var Twitter = require('./twitter');
+var config = {}
 
-var twitterClient = new twitterClient({
+//TWITTER CONFIG
+config.ConsumerKey = 'q2JfZwE8BTIHHNruzRqaWubEI';
+config.ConsumerSecret = 'FI4nM1BuayKsMBLZ2fA4zicaOkNUvjrhfRgiDYngVQVBtkXRyT';
+config.AccessToken = '242907489-B4Xul8Rik2fzwnLrhim6pQEElnLZYLNMHXAeuDn3';
+config.AccessTokenSecret = 'KAQjh2M9I2Pf6BUrwoF4QsYrb0wXO696UlmGeOscMLrGj';
+config.HashTag = 'test';
+
+var twitterClient = new Twitter({
     consumer_key: config.ConsumerKey,
     consumer_secret: config.ConsumerSecret,
     access_token: config.AccessToken,
@@ -9,16 +16,18 @@ var twitterClient = new twitterClient({
     timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
 });
 
+var stream = twitterClient.stream('statuses/filter', {
+    track: [config.HashTag]
+});
+
 module.exports = function (context) {
-
-    var stream = twitterClient.stream('statuses/filter', {
-        track: [config.HashTag]
-    });
-
+    
     stream.on('tweet', function (tweet) {
-        context.bindings.myOutput = {
+        var data = {
             content: tweet,
             type: 1
         };
+        
+        context.done(null, data);
     });
 };
