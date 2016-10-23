e7f20717e08b373e41c58f739f8760ff2caf88b3
Jef King <jefkin@microsoft.com>
2016-10-23 08:00:45 -0600

    add to test

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,5 +1,16 @@
 var DocumentDBClient = require('documentdb').DocumentClient;
 
+var config = {
+    DatabaseId: "missingdata",
+    FacebookCollectionId: "facebook-profile",
+    TwitterCollectionId: "twitter-profile",
+    Host: process.env.DocDb_Host,
+    AuthKey: process.env.DocDb_AuthKey,
+};
+config.collLink = 'dbs/' + config.DatabaseId + '/colls/' + config.TwitterCollectionId
+
+var docDbClient = new DocumentDBClient(config.Host, { masterKey: config.AuthKey });
+
 module.exports = function (context, message) {
     //fliter on twitter handle
     context.bindings.out = message;
