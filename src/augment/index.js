6dae717c21d7713be4064c5aabffabfe59e8a43d
Jef King <jefkin@microsoft.com>
2016-10-23 08:40:11 -0600

    Comments

--- src/augment/index.js
+++ src/augment/index.js
@@ -1,7 +1,9 @@
 module.exports = function(context, message) {
-    //add more data
+    //Pull Lat/Long from Twitter, based on Tweet
     message.latitude = -115.00002;
     message.longitude = 41.94000;
+
+    //Pull in X # of Photos from Twitter
     message.photos = [];
 
     context.bindings.out = message;
