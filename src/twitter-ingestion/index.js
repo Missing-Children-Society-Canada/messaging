5e20b0209941df5aee6965c010a9eadc11227785
Jef King <jefkin@microsoft.com>
2016-10-22 18:01:02 -0600

    MIS

--- src/twitter-ingestion/index.js
+++ src/twitter-ingestion/index.js
@@ -1,17 +1,17 @@
 var twitterClient = require('./twitter');
 var config = require('./config');
 
-var T = new twitterClient({
-    consumer_key: config.ConsumerKey,
-    consumer_secret: config.ConsumerSecret,
-    access_token: config.AccessToken,
-    access_token_secret: config.AccessTokenSecret,
-    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-});
-
 module.exports = function (context) {
 
-    var stream = T.stream('statuses/filter', {
+    var twitterClient = new twitterClient({
+        consumer_key: config.ConsumerKey,
+        consumer_secret: config.ConsumerSecret,
+        access_token: config.AccessToken,
+        access_token_secret: config.AccessTokenSecret,
+        timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    });
+
+    var stream = twitterClient.stream('statuses/filter', {
         track: [config.HashTag]
     })
 
