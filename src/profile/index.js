0d0a5001eb66015a82e79ef87e7e8cfcc7dba89a
Jef King <jefkin@microsoft.com>
2017-05-12 13:26:24 -0700

    again!

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,18 +1,30 @@
-var sql = require('mssql');
+const sql = require('mssql');
+const config = {
+    server: "mcscroot.database.windows.net", // Use your SQL server name
+    database: "social", // Database to connect to
+    user: "mcscroot", // Use your username
+    password: "mqRJAxoRd1lvlS1N1UVuhn220OzT0d", // Use your password
+    port: 1433,
+    // Since we're on Windows Azure, we need to set the following options
+    options: {
+        encrypt: true
+    }
+};
 
 module.exports = function (context, req) {
+    sql.connect(config).then(pool => {
+        return pool.request()
+            .query('select * from [dbo].[vwProfiles]')
+    }).then(result => {
+        context.res = {
+            status: 200,
+            body: result
+        };
+    }).catch(err => {
+        context.log(err);
+    })
 
-    return new Promise(function (fulfill, reject) {
-
-        var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true', err => {
-            sql.query(`select * from [dbo].[vwProfiles]`, (err, result) => {
-                context.res = {
-                    status: 200,
-                    body: result
-                };
-            });
-        });
-
-        sql.close();
-    });
+    sql.on('error', err => {
+        context.log(err);
+    })
 }
