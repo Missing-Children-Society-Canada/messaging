b3067ce339e46168f55d286d69efb9cd29e2e956
Jef King <jefkin@microsoft.com>
2017-05-11 17:35:36 -0700

    config

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,16 +1,17 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 
+var config = {
+    userName: 'mcscroot',
+    password: 'mqRJAxoRd1lvlS1N1UVuhn220OzT0d',
+    server: 'mcsc.database.windows.net',
+    options: {
+        database: 'social'
+    }
+}
+
 module.exports = function (context, req) {
 
-    var config = {
-        userName: 'mcscroot',
-        password: 'mqRJAxoRd1lvlS1N1UVuhn220OzT0d',
-        server: 'mcsc.database.windows.net',
-        options: {
-            database: 'social'
-        }
-    }
     var connection = new Connection(config);
 
     connection.on('connect', function (err) {
