a7a306aaa024f54932ea93634f63a6c5243c51b1
Jef King <jefkin@microsoft.com>
2017-05-12 12:27:18 -0700

    misc

--- src/profile/index.js
+++ src/profile/index.js
@@ -4,8 +4,8 @@ module.exports = function (context, req) {
 
     try {
 
-        var pool = await sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
-        var result = await sql.query`select * from [dbo].[vwProfiles]`
+        var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
+        var result = sql.query(`select * from [dbo].[vwProfiles]`)
         context.res = {
             status: 200,
             body: result
