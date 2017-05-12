5206cdd6011dd5ce2c2913272658309b51bee395
Jef King <jefkin@microsoft.com>
2017-05-12 12:41:35 -0700

    oops remove await

--- src/profile/index.js
+++ src/profile/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
 
     var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
         .then(function () {
-            var result = await sql.query(`select * from [dbo].[vwProfiles]`)
+            var result = sql.query(`select * from [dbo].[vwProfiles]`)
                 .then(function (result) {
                     context.res = {
                         status: 200,
