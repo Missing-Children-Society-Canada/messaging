9328d1e38262b2b5b1b47b8f6fa03e69f43c0bf9
Jef King <jefkin@microsoft.com>
2017-05-12 12:40:54 -0700

    promise

--- src/profile/index.js
+++ src/profile/index.js
@@ -2,18 +2,16 @@ var sql = require('mssql');
 
 module.exports = function (context, req) {
 
-    try {
-
-        var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
-        var result = sql.query(`select * from [dbo].[vwProfiles]`)
-        context.res = {
-            status: 200,
-            body: result
-        };
-
-    } catch (err) {
-        // ... error checks 
-    }
+    var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
+        .then(function () {
+            var result = await sql.query(`select * from [dbo].[vwProfiles]`)
+                .then(function (result) {
+                    context.res = {
+                        status: 200,
+                        body: result
+                    };
+                });
+        });
 
     context.done();
 }
