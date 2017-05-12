40873a67ece58a4b3929f69b1668c051d5c50421
Jef King <jefkin@microsoft.com>
2017-05-12 12:44:40 -0700

    syntax

--- src/profile/index.js
+++ src/profile/index.js
@@ -2,16 +2,14 @@ var sql = require('mssql');
 
 module.exports = function (context, req) {
 
-    var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
-        .then(function () {
-            var result = sql.query(`select * from [dbo].[vwProfiles]`)
-                .then(function (result) {
-                    context.res = {
-                        status: 200,
-                        body: result
-                    };
-                });
+    var pool = sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true', err => {
+        var result = sql.query(`select * from [dbo].[vwProfiles]`, (err, result) => {
+            context.res = {
+                status: 200,
+                body: result
+            };
         });
+    });
 
     context.done();
 }
