09eb2062501071e1f4b4eee297e67dd2b8b6e733
Jef King <jefkin@microsoft.com>
2017-05-11 18:14:37 -0700

    issues in my brain

--- src/profile/index.js
+++ src/profile/index.js
@@ -21,25 +21,24 @@ module.exports = function (context, req) {
                 body: err
             }
         }
-        else {
-            var request = new Request("SELECT * FROM [dbo].[vwProfiles]",
-                function (err, rowCount, rows) {
-                    if (err) {
-                        context.res = {
-                            status: 500,
-                            body: err
-                        }
-                    }
-                    else {
-                        context.res = {
-                            status: 200,
-                            body: "WTF"//rows
-                        };
-                    }
-                });
-        }
     });
 
+    var request = new Request("SELECT * FROM [dbo].[vwProfiles]",
+        function (err, rowCount, rows) {
+            if (err) {
+                context.res = {
+                    status: 500,
+                    body: err
+                }
+            }
+            else {
+                context.res = {
+                    status: 200,
+                    body: "WTF"//rows
+                };
+            }
+        });
+        
     connection.execSql(request);
 
     context.done();
