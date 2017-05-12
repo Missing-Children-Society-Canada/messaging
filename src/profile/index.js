e405bcd17f523bb1696d7786273d5a8735d28346
Jef King <jefkin@microsoft.com>
2017-05-11 17:20:49 -0700

    switch package

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,4 +1,4 @@
-var sql = require('msnodesql');
+var sql = require('tedious');
 
 module.exports = function (context, req) {
     var conn_str = "Driver={SQL Server Native Client 11.0};Server=tcp:mcsc.database.windows.net;UID=mcscroot;PWD=mqRJAxoRd1lvlS1N1UVuhn220OzT0d;Database=social;";
