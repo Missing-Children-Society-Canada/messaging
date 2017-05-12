c39a5ff9ea61fa109f286ca58334db93b350b50f
Jef King <jefkin@microsoft.com>
2017-05-12 12:24:53 -0700

    var

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,14 +1,11 @@
 var sql = require('mssql');
 
-
-//var conn_str = "Driver={SQL Server Native Client 11.0};Server=tcp:mcscroot.database.windows.net;UID=mcscroot;PWD=mqRJAxoRd1lvlS1N1UVuhn220OzT0d;Database=social;";
-
 module.exports = function (context, req) {
 
     try {
 
-        const pool = await sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
-        const result = await sql.query`select * from [dbo].[vwProfiles]`
+        var pool = await sql.connect('mssql://mcscroot:mqRJAxoRd1lvlS1N1UVuhn220OzT0d@mcscroot.database.windows.net/social?encrypt=true')
+        var result = await sql.query`select * from [dbo].[vwProfiles]`
         context.res = {
             status: 200,
             body: result
