93cdf23e424256e13087e5f68529d46d1b15769f
Jef King <jefkin@microsoft.com>
2017-05-12 12:13:15 -0700

    constructor issue

--- src/profile/index.js
+++ src/profile/index.js
@@ -1,20 +1,22 @@
 var sql = require('mssql');
 
-var config = {
-    server: "mcscroot.database.windows.net", // Use your SQL server name
-    database: "social", // Database to connect to
-    user: "mcscroot", // Use your username
-    password: "mqRJAxoRd1lvlS1N1UVuhn220OzT0d", // Use your password
-    port: 1433,
-    // Since we're on Windows Azure, we need to set the following options
-    options: {
-        encrypt: true
-    }
-};
 
 //var conn_str = "Driver={SQL Server Native Client 11.0};Server=tcp:mcscroot.database.windows.net;UID=mcscroot;PWD=mqRJAxoRd1lvlS1N1UVuhn220OzT0d;Database=social;";
 
 module.exports = function (context, req) {
+
+    var config = {
+        server: "mcscroot.database.windows.net", // Use your SQL server name
+        database: "social", // Database to connect to
+        user: "mcscroot", // Use your username
+        password: "mqRJAxoRd1lvlS1N1UVuhn220OzT0d", // Use your password
+        port: 1433,
+        // Since we're on Windows Azure, we need to set the following options
+        options: {
+            encrypt: true
+        }
+    };
+    
     var conn = new sql.Connection(config);
 
     conn.connect()
