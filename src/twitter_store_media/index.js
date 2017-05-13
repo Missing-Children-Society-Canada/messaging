a339d3fdf4bca468efaff7fd4e0357c7f3b2aad2
Jef King <jefkin@microsoft.com>
2017-05-13 14:34:26 -0700

    add

---
+++ src/twitter_store_media/index.js
@@ -0,0 +1,27 @@
+const sql = require('mssql');
+
+const config = {
+    server: process.env.SqlServer,
+    database: process.env.SqlDatabase,
+    user: process.env.SqlUser,
+    password: process.env.SqlPassword,
+    port: 1433,
+    options: {
+        encrypt: true
+    }
+};
+
+module.exports = function (context, message) {
+
+    const request = new sql.Request()
+    request.input('Identifier', sql.UniqueIdentifier, message.tweetid || message.media_id);
+    request.input('AccountId', sql.NvarChar, message.userid);
+    request.input('MediaId', sql.NvarChar, content.id);
+    request.input('CreatedOn', sql.DateTime, new Date());
+    request.input('LastUpdatedOn', sql.DateTime, new Date());
+    request.input('Content', sql.NVarChar, message.tweettext || message.media_text);
+
+    request.execute('[dbo].[StoreSocialInteraction]');
+
+    context.done();
+};
