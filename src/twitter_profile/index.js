feb7327a4e5ebd1076e86bdb6e4dcb22cdf977bc
Jef King <jefkin@microsoft.com>
2017-05-13 19:44:28 -0700

    pushing messages

--- src/twitter_profile/index.js
+++ src/twitter_profile/index.js
@@ -1,5 +1,21 @@
 var util = require('util');
 var twitter = require('twitter');
 
-module.exports = function (context, req) {
+module.exports = function (context, message) {
+
+    context.log(message);
+
+    return twit.get(`users/show.json?screen_name=${message.twitter.username}`)
+        .then(setOutputBinding)
+        .then(log)
+
+    function setOutputBinding(data) {
+        context.bindings.out = data;
+        return data;
+    }
+
+    function setOutputBinding(data) {
+        context.log(data);
+        return data;
+    }
 }
