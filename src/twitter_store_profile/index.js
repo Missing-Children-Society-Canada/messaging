e73186de7ac7bbcb625c3ed023c0164ea3e219f0
Jef King <jefkin@microsoft.com>
2017-05-13 19:46:27 -0700

    closer

--- src/twitter_store_profile/index.js
+++ src/twitter_store_profile/index.js
@@ -1,2 +1,19 @@
+const sql = require('mssql');
+
+const config = {
+    server: process.env.SqlServer,
+    database: process.env.SqlDatabase,
+    user: process.env.SqlUser,
+    password: process.env.SqlPassword,
+    port: 1433,
+    options: {
+        encrypt: true
+    }
+};
+
 module.exports = function (context, req) {
+    
+    const request = new sql.Request();
+
+    context.done();
 }
