30efe09b2ca4c4a06950398c14efa05de187d8f8
Jef King <jefkin@microsoft.com>
2017-05-13 20:33:57 -0700

    include entities

--- src/twitter_profile/index.js
+++ src/twitter_profile/index.js
@@ -10,7 +10,7 @@ const twit = new twitter({
 
 module.exports = function (context, message) {
 
-    return twit.get(`users/show.json?user_id=${message.twitter.id}`)
+    return twit.get(`users/show.json?user_id=${message.twitter.id}`, { include_entities: true })
         .then(log)
 
     function setOutputBinding(data) {
