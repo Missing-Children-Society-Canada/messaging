736533ec419e0ff23b249bd40fd40c0032e651ec
Jef King <jefkin@microsoft.com>
2017-05-13 19:50:00 -0700

    fix

--- src/twitter_profile/index.js
+++ src/twitter_profile/index.js
@@ -1,6 +1,13 @@
 var util = require('util');
 var twitter = require('twitter');
 
+const twit = new twitter({
+    consumer_key: process.env.TwitterConsumerKey,
+    consumer_secret: process.env.TwitterConsumerSecret,
+    access_token_key: process.env.TwitterAccessTokenKey,
+    access_token_secret: process.env.TwitterAccessTokenSecret
+});
+
 module.exports = function (context, message) {
 
     context.log(message);
