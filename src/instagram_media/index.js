90f1fe592549467a02d02d230a146a075608910e
Jef King <jefkin@microsoft.com>
2017-05-14 14:21:01 -0700

    Instagram Media Pipeline done

--- src/instagram_media/index.js
+++ src/instagram_media/index.js
@@ -1,21 +1,15 @@
 let ig = require('instagram-node');
-let providers = require('..\constants').providers;
 
-module.exports = function (context, msg) {
-    if (context.bindings.token.length) {
-        let token = context.bindings.token[0].token;
-        let client = ig.instagram();
-        client.use({ access_token: token });
-        client.media(msg.media_id, handleMedia);
-    } else {
-        context.log("user token not found");
-        context.done();
-    }
+module.exports = function (context, message) {
+    let token = message.instagram.token;
+    let client = ig.instagram();
+    client.use({ access_token: token });
+    client.media(msg.media_id, handleMedia);
 
     function handleMedia(err, media) {
         if (media.tags.includes('hfm')) {
             let message = {
-                social_site: providers.instagram,
+                plaftorm: 'instagram',
                 userid: msg.user_id,
                 longitude: media.location.longitude,
                 latitude: media.location.latitude,
@@ -25,11 +19,11 @@ module.exports = function (context, msg) {
                 media_text: media.caption.text
             }
 
-            context.log('added to store: ', media.id);
             context.bindings.out = message;
         } else {
             context.log('not #hfm: ', media.id);
         }
+
         context.done(err);
     }
 };
--- src/instagram_profile/index.js
+++ src/instagram_profile/index.js
@@ -1,2 +1,4 @@
+let ig = require('instagram-node');
+
 module.exports = function (context, req) {
 }
