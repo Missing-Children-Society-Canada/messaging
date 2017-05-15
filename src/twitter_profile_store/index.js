1b5f02ebcb0392d36fa511941188d82f32565cf6
Jef King <jefkin@microsoft.com>
2017-05-15 15:50:04 -0700

    profile store

--- src/twitter_profile_store/index.js
+++ src/twitter_profile_store/index.js
@@ -1,6 +1,19 @@
 module.exports = function (context, message) {
-    let err = null;
-    let data = message;
-    
-    context.done(err, data);
+
+    context.bindings.out = JSON.stringify({
+        id: guid(),
+        message: message
+    });
+
+    context.done();
+
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    }
 }
