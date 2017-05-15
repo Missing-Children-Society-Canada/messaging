2830d7767a6f800c7c02a292356836c980700409
Jef King <jefkin@microsoft.com>
2017-05-15 16:29:32 -0700

    get profile

--- src/instagram_profile_augment/index.js
+++ src/instagram_profile_augment/index.js
@@ -1,25 +1,15 @@
 let ig = require('instagram-node');
 
 module.exports = function (context, message) {
-    
+
     context.log(message.instagram.token);
 
     let client = ig.instagram();
     client.use({ access_token: message.instagram.token });
 
-    client.media(message.mediaid, handleMedia); //USED FOR FILTERING; Would be better upstream???
-
-    function handleMedia(err, media) {
-        if (media.tags.includes('hfm')) {
-            ig.user('user_id', handleProfile);
-        } else {
-            context.log('not #hfm: ', media.id);
-        }
-    }
-
-    function handleProfile(err, result) {
+    ig.user(message.instagram.id, function (err, result) {
         context.bindings.out = result;
 
         context.done(err);
-    }
+    });
 }
