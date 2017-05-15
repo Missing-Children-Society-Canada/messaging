59b122a9b3af123c7a34b31a1864c4e5145f8117
Jef King <jefkin@microsoft.com>
2017-05-15 14:27:43 -0700

    log twitter token

--- src/twitter_profile_augment/index.js
+++ src/twitter_profile_augment/index.js
@@ -1,16 +1,16 @@
 var util = require('util');
 var twitter = require('twitter');
 
-const twit = new twitter({
-    consumer_key: process.env.TwitterConsumerKey,
-    consumer_secret: process.env.TwitterConsumerSecret,
-    access_token_key: process.env.TwitterAccessTokenKey,
-    access_token_secret: process.env.TwitterAccessTokenSecret
-});
-
 module.exports = function (context, message) {
 
     context.log(message.twitter.id);
+    context.log(message.twitter.token);
+
+    const twit = new twitter({
+        consumer_key: process.env.TwitterConsumerKey,
+        consumer_secret: process.env.TwitterConsumerSecret,
+        bearer_token: message.twitter.token
+    });
 
     return twit.get(`users/show.json?user_id=${message.twitter.id}`, { include_entities: true })
         .then(log)
