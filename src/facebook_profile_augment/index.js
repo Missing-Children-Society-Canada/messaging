935ba87b36eafcfed01580a772572b3278288dc7
Jef King <jefkin@microsoft.com>
2017-05-16 09:15:11 -0700

    making fat

--- src/facebook_profile_augment/index.js
+++ src/facebook_profile_augment/index.js
@@ -3,20 +3,26 @@ var request = require('request-promise');
 module.exports = function (context, message) {
 
     // connect to facebook graph
-    let myPath = '/' + message.facebook.id + '?fields=email,first_name,last_name,birthday,locale,location,picture.width(500),about,education,friends,hometown,photos,relationship_status,religion,political,tagged_places,work';
+    let myPath = '/' + message.request.facebook.id + '?fields=email,first_name,last_name,birthday,locale,location,picture.width(500),about,education,friends,hometown,photos,relationship_status,religion,political,tagged_places,work';
 
     let options = {
         uri: "https://graph.facebook.com" + myPath,
         method: 'GET',
         headers: {
-            'Authorization': 'OAuth ' + message.facebook.token
+            'Authorization': 'OAuth ' + message.request.facebook.token
         }
     };
 
     request(options)
         .then((response) => {
             let data = message;
-            message.facebook.profile = response;
+            
+            message.response = {
+                platform: "facebook",
+                type: "profile",
+                data: response
+            };
+
             context.bindings.out = data;
         })
         .catch((error) => context.log(error))
