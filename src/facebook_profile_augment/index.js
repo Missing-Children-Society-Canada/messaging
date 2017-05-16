c6782842324560a11f7bc183954f27904fc88142
Jef King <jefkin@microsoft.com>
2017-05-16 08:58:41 -0700

    fb binding

--- src/facebook_profile_augment/index.js
+++ src/facebook_profile_augment/index.js
@@ -1,15 +1,15 @@
 var request = require('request-promise');
 
-module.exports = function (context, inMsg) {
+module.exports = function (context, message) {
 
     // connect to facebook graph
-    let myPath = '/' + inMsg.fb_user_id + '?fields=email,first_name,last_name,birthday,locale,location,picture.width(500),about,education,friends,hometown,photos,relationship_status,religion,political,tagged_places,work';
+    let myPath = '/' + message.facebook.id + '?fields=email,first_name,last_name,birthday,locale,location,picture.width(500),about,education,friends,hometown,photos,relationship_status,religion,political,tagged_places,work';
 
     let options = {
         uri: "https://graph.facebook.com" + myPath,
         method: 'GET',
         headers: {
-            'Authorization': 'OAuth ' + inMsg.fb_access_token
+            'Authorization': 'OAuth ' + message.facebook.token
         }
     };
 
