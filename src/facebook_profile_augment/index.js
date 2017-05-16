6338e54df720e23ca273451635d79b3f5ce2a667
Joe Raio <joe.raio@microsoft.com>
2017-05-15 22:17:40 -0700

    facebook profile augment function

--- src/facebook_profile_augment/index.js
+++ src/facebook_profile_augment/index.js
@@ -1,2 +1,29 @@
-module.exports = function (context, req) {
+var request = require('request-promise');
+
+module.exports = function (context, inMsg) {
+
+    // log the message for debugging
+    context.log('UserId: ',inMsg.fb_user_id);
+
+    // connect to facebook graph
+    var myPath = '/' + inMsg.fb_user_id + '?fields=email,first_name,last_name,birthday,locale,location,picture.width(500),about,education,friends,hometown,photos,relationship_status,religion,political,tagged_places,work';
+    
+    var options = {
+        uri: "https://graph.facebook.com" + myPath,
+        method: 'GET',
+        headers: {
+            'Authorization': 'OAuth ' + inMsg.fb_access_token
+        }
+    };    
+
+    request(options)
+        .then((response) => {
+
+            context.log("From FB:", response);
+            context.bindings.outMsg = response;
+
+        })
+        .catch((error) => context.log(error))
+        .finally(() => context.done());
+
 }
