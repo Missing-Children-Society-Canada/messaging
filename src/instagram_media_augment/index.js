cffcc2af07b41823b2eebcf463a4c713bed74968
Jef King <jefkin@microsoft.com>
2017-05-15 18:13:02 -0700

    instagram

--- src/instagram_media_augment/index.js
+++ src/instagram_media_augment/index.js
@@ -3,18 +3,13 @@ let ig = require('instagram-node');
 module.exports = function (context, message) {
     let token = message.instagram.token;
     let client = ig.instagram();
-    client.use({ access_token: token });
-    client.media(message.mediaid, function(err, media) {
-        let message = {
-            plaftorm: 'instagram',
-            userid: msg.user_id,
-            longitude: media.location.longitude,
-            latitude: media.location.latitude,
-            profileimageurl: media.user.profile_picture,
-            media_id: media.id,
-            media_text: media.caption.text
-        }
 
-        context.done(err, message);
+    client.use({ access_token: token });
+
+    client.media(message.mediaid, function (err, media) {
+
+        context.bindings.out = media;
+
+        context.done(err);
     });
 };
