0c4b08cae2497dd51bfdde3fd24974de1f217598
Marc GAgne <marc.gagne@hotmail.com>
2017-05-17 14:46:29 -0400

    adding more fields to FB grpah call

--- src/facebook_media_augment/index.js
+++ src/facebook_media_augment/index.js
@@ -2,7 +2,7 @@ var request = require('request-promise');
 
 module.exports = function (context, message) {
 
-    let path = '/' + message.request.mediaid + '?fields=id,message,message_tags,picture,place,name,comments,reactions';
+    let path = '/' + message.request.mediaid + '?fields=id,message,message_tags,application,created_time,picture,place,name,comments,reactions,object_id,privacy,properties,shares,status_type,story,story_tags,updated_time,with_tags';
 
     let options = {
         uri: "https://graph.facebook.com" + path,
