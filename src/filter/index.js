80eca91e498df7cbea9532b087731f436cb2187d
Joe Raio <joe.raio@microsoft.com>
2017-07-27 14:14:35 -0400

    Fixes #14 - Refactored FILTER to use bindding for query

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,53 +1,42 @@
-const DocumentDBClient = require('documentdb').DocumentClient;
-
-const config = {
-    Host: process.env.DocDb_Host,
-    AuthKey: process.env.DocDb_AuthKey,
-};
-
 module.exports = function (context, message) {
-
+    
     let err = null;
 
-    const docDbClient = new DocumentDBClient(config.Host, { masterKey: config.AuthKey });
-    const query = 'SELECT * FROM c WHERE (c.twitter[\'$id\'] = \'' + message.userid + '\' AND \'twitter\' = \'' + message.platform + '\') OR (c.instagram[\'$id\'] = \'' + message.userid + '\' AND \'instagram\' = \'' + message.platform + '\') OR (c.facebook[\'$id\'] = \'' + message.userid + '\' AND \'facebook\' = \'' + message.platform + '\')';
+    var documents = context.bindings.documents;
 
-    docDbClient.queryDocuments('dbs/user/colls/socials', query).toArray(function (err, results) {
+    let userdata = documents[0];
+    
+    if (!userdata || userdata == undefined) {
+        context.log('Not tracking user');
 
-        let userdata = results[0];
+        err = new Error('Not tracking user');
+        return;
+    }
 
-        if (!userdata || userdata == undefined) {
-            context.log('Not tracking user');
+    if ((!userdata.twitter || userdata.twitter == undefined)
+        && (!userdata.facebook || userdata.facebook == undefined)
+        && (!userdata.instagram || userdata.instagram == undefined)) {
+        context.log('No social profiles');
 
-            err = new Error('Not tracking user');
-            return;
-        }
+        err = new Error('No social profiles');
+        return;
+    }
 
-        if ((!userdata.twitter || userdata.twitter == undefined)
-            && (!userdata.facebook || userdata.facebook == undefined)
-            && (!userdata.instagram || userdata.instagram == undefined)) {
-            context.log('No social profiles');
+    if (!userdata.id) {
+        context.log('No user id');
 
-            err = new Error('No social profiles');
-            return;
-        }
+        err = new Error('No social profiles');
+        return;
+    }
 
-        if (!userdata.id) {
-            context.log('No user id');
+    context.bindings.out = {
+        requestid: guid(),
+        user: userdata,
+        request: message,
+        triggeredOn: new Date()
+    };
 
-            err = new Error('No social profiles');
-            return;
-        }
-
-        context.bindings.out = {
-            requestid: guid(),
-            user: userdata,
-            request: message,
-            triggeredOn: new Date()
-        };
-
-        context.done(err);
-    });
+    context.done(err);
 
     function guid() {
         function s4() {
